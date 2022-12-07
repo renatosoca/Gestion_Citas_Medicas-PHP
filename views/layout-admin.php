7b79fd686b9fe3f78757eb8bd35d1a98738e2b80
jxdflores25 <jxdflores25@gmail.com>
2022-12-06 23:12:43 -0500

    a

--- views/layout-admin.php
+++ views/layout-admin.php
@@ -15,6 +15,9 @@
     <script src="https://kit.fontawesome.com/b355a0cb3a.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <!-- Custom styles for this template-->
     <link href="/build/css/sb-admin-2.min.css" rel="stylesheet">
 
