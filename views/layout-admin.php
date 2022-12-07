b53b3056510ad247564989f041605ec25d588b02
jxdflores25 <jxdflores25@gmail.com>
2022-12-06 23:16:32 -0500

    a

--- views/layout-admin.php
+++ views/layout-admin.php
@@ -143,8 +143,11 @@
         </div>
     </div>
 
-    <!-- Custom scripts for all pages-->
-    <script src="/build/js/sb-admin-2.min.js"></script>
+     <!-- Custom scripts for all pages-->
+     <script src="/build/js/sb-admin-2.min.js"></script>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 
 </body>
